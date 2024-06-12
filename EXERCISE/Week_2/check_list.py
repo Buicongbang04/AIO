dc6d323085e176c8a4feebd5ddd5f78f05590498
Buicongbang04 <buicongbang1010@gmail.com>
2024-06-12 19:52:21 +0700

    update sonar and exercise week 2

---
+++ EXERCISE/Week_2/check_list.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def checkList(num_list: list, k: int):
+    for i in range(len(num_list) - k + 1):
+        max_num = max(num_list[i:i+k])
+        print(f"max {max_num}")
+
+
+if __name__ == "__main__":
+    num_list = [3, 4, 5, 1, -44, 5, 10, 12, 33, 1]
+    k = 3
+    checkList(num_list, k)
---
+++ EXERCISE/Week_2/read_file.py
@@ -0,0 +1,14 @@
+def wordCount(url: str):
+    result = dict()
+    with open(url, 'r') as file:
+        data = file.read()
+        for word in data.split():
+            if word in result:
+                result[word] += 1
+            else:
+                result[word] = 1
+    return result
+
+
+if __name__ == '__main__':
+    print(wordCount('D:\Study\AI-Book\AIO\EXERCISE\Week_2\data\P1_data.txt'))
